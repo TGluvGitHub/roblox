72d03df444b332a38fa76b15fb7e5a58cc938c74
TGluvGitHub <108703551+TGluvGitHub@users.noreply.github.com>
2022-07-05 12:22:29 -0700

    Add files via upload

---
+++ RBLX.py
@@ -0,0 +1,9 @@
+game_load_a = "ivarg"
+print(game_load_a)
+ingame_username = "ivargforpy.av"
+print(ingame_username)
+class StartGame:
+    game_load_b = "py.load.send=game_load_a]"
+    gameid = "978910387"
+    print(gameid)
+    print(game_load_b)
